f8e9e78184e15c515b516fe75340702ef8727100
JINAY2910 <jinay2910@gmail.com>
2025-11-22 16:34:39 +0530

    Add vercel-url in cors

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"]
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://qyra-gamma.vercel.app"]
 })
 );
 app.use(express.json());
